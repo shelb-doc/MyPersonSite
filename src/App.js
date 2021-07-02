df7f42db2097ae7c5f02c54caf92d8ca3e4bf6fe
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-07-02 12:16:54 -0400

    added a footer componet

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -13,6 +14,8 @@ export default function App() {
       <Projects />
       <Skills />
       <Contact />
+      <Footer />
+
     </main>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,17 +29,26 @@ return (
     <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
     <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
         <div className="bg-gray-800 relative flex flex-wrap py-6 rounded shadow-md">
-        <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+        <div className="xl:w-1/2 px-6 mt-4 lg:mt-0">
             <h2 className="title-font font-semibold text-white tracking-widest text-xs">
             EMAIL
             </h2>
             <a className="text-indigo-400 leading-relaxed">
             Shelbycignetti@gmail.com
             </a>
+            
+            <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4 ">
+            LINKEDIN
+            </h2>
+            <a className="text-indigo-400 leading-relaxed">
+            https://www.linkedin.com/in/shelbycignetti/
+            </a>
+            
             <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
             PHONE
             </h2>
             <p className="leading-relaxed">770-668-6431</p>
+
         </div>
         </div>
     </div>
@@ -52,7 +61,7 @@ return (
         Hire Me
         </h2>
         <p className="leading-relaxed mb-5"> Work Authorization = US Citizen
-        and able to work Full-time, Contract - W2, Contract to Hire - Corp-to-Corp, Contract to Hire - Independent, Contract to Hire - W2, Internship
+        &amp; able to work Full-time, Contract - W2, Contract to Hire - Corp-to-Corp, Contract to Hire - Independent, Contract to Hire - W2, Internship
         </p>
         <div className="relative mb-4">
         <label htmlFor="name" className="leading-7 text-sm text-gray-400">
