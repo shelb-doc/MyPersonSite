fb993722cf6eabdc8656beddb4f0bef86aa8d93b
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-08-26 19:20:59 -0400

    updated blog

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Projects from "./components/Projects";
 import Art from "./components/Designs";
 import Skills from "./components/Skills";
 import Footer from "./components/Footer";
+import Blog from "./components/Blog";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <About />
       <Projects />
       <Art />
+      <Blog />
       <Skills />
       <Contact />
       <Footer />
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { TemplateIcon } from "@heroicons/react/solid";
 
 export default function Blog() {
 return (
 <section id="blog">
+    <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+    <div className="flex flex-col w-full mb-20">
+        <TemplateIcon className="mx-auto inline-block w-10 mb-4" />
+        <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+        🚧Dev Journal *UNDER CONSTRUCTION*🚧
+        </h1>
+        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+        just an update life talk for me
+        </p>
+    </div>
+
+    </div>
 
 </section>
 );
