ce172d99ebdcc34ccba4b6f861534e8e09604a36
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-08-26 19:41:20 -0400

    updated icons

--- src/components/Designs.js
+++ src/components/Designs.js
@@ -1,4 +1,4 @@
-import { CodeIcon } from "@heroicons/react/solid";
+import { TemplateIcon } from "@heroicons/react/solid";
 import React from "react";
 import { designs } from "../data";
 
@@ -7,7 +7,7 @@ return (
 <section id="Designs" className="text-gray-400 bg-gray-900 body-font">
     <div className="container px-5 py-10 mx-auto text-center lg:px-40">
     <div className="flex flex-col w-full mb-20">
-        <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+        <TemplateIcon className="mx-auto inline-block w-10 mb-4" />
         <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
         🚧Designs and Art *UNDER CONSTRUCTION*🚧
         </h1>
